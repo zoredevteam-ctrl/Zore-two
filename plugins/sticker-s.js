a7f733787edd12045a62ad301f2a9906b0f0c16e
yosue891 <yosueortega630@gmail.com>
2026-02-24 14:25:14 -0400

    Update sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -1,7 +1,5 @@
 import { sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
+import { downloadMediaMessage } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn, args }) => {
     let stiker = false
@@ -9,30 +7,27 @@ let handler = async (m, { conn, args }) => {
     let packstickers = global.db.data.users[userId] || {}
 
     let texto2 = packstickers.text2 || "🩷 Zero Two — 002"
-    let texto1 = ''
-    
-    if (args.length > 0) {
-        texto1 = args.join(' ').trim() || "Zero Two"
-    } else {
-        texto1 = "Zero Two"
-    }
+    let texto1 = args.length > 0 ? args.join(' ').trim() : "Zero Two"
 
     try {
         let q = m.quoted ? m.quoted : m
-        let mime = (q.msg || q).mimetype || q.mediaType || ''
+        let mime = q.mimetype || q.msg?.mimetype || ''
 
-        if (/webp|image|video/g.test(mime) && q.download) {
-            if (/video/.test(mime) && (q.msg || q).seconds > 16)
+        if (/image|video|webp/.test(mime)) {
+
+            if (/video/.test(mime) && q.seconds > 16)
                 return conn.reply(m.chat, '🩷 Darling… ese video es muy largo. Debe durar *menos de 15 segundos*.', m)
 
-            let buffer = await q.download()
             await m.react('💗')
+
+            let buffer = await downloadMediaMessage(q, 'buffer', {})
             let marca = [texto1, texto2]
+
             stiker = await sticker(buffer, false, marca[0], marca[1])
 
         } else if (args[0] && isUrl(args[0])) {
-            let buffer = await sticker(false, args[0], texto1, texto2)
-            stiker = buffer
+
+            stiker = await sticker(false, args[0], texto1, texto2)
 
         } else {
             return conn.reply(m.chat, '🩷 Envíame una imagen o video, Darling… haré un sticker adorable para ti.', m)
@@ -44,7 +39,7 @@ let handler = async (m, { conn, args }) => {
 
     } finally {
         if (stiker) {
-            conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+            await conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
             await m.react('💞')
         }
     }
@@ -57,5 +52,5 @@ handler.command = ['s', 'sticker']
 export default handler
 
 const isUrl = (text) => {
-    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)(jpe?g|gif|png)/, 'gi'))
-          }
+    return text.match(/https?:\/\/\S+\.(jpg|jpeg|png|gif)/gi)
+        }
