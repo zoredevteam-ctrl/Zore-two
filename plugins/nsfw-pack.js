d6d34de037933ec1dc811c2721aa1a7be827db4e
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-02-24 19:02:44 -0500

    Actualizar nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -6,7 +6,7 @@ m.react('🕑');
 
 const gp = global.db.data.chats[m.chat] || {};
 
-if (!gp.nsfw && m.isGroup) return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
+
 
 let txt = 'Pack🔥🔥🔥';
 
