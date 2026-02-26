8f8b4b57a90f919acef96e1adfd065731f7e5121
Carlos <supernumerovirtual@gmail.com>
2026-02-25 19:12:58 -0500

    Crear descargas-pindl.js

---
+++ plugins/descargas-pindl.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+
+const handler = async (msg, { conn, args, usedPrefix, command }) => {
+  const url = args[0]
+
+  if (!url) {
+    await conn.sendMessage(
+      msg.chat,
+      { text: `❌ *Error:*\n> Debes enviar un enlace de Pinterest.` },
+      { quoted: msg }
+    )
+
+    return conn.sendMessage(
+      msg.chat,
+      { text: `✳️ Usa:\n${usedPrefix}pindl <link de pinterest>` },
+      { quoted: msg }
+    )
+  }
+
+  await conn.sendMessage(
+    msg.chat,
+    { text: '*📌 Descargando Pinterest...*' },
+    { quoted: msg }
+  )
+
+  try {
+    const api = `https://nexevo-api.vercel.app/download/pinterest?url=${encodeURIComponent(url)}`
+    const { data } = await axios.get(api)
+
+    if (!data?.status || !data?.result?.dl)
+      throw new Error('Error en la descarga.')
+
+    const title = data.result.titulo || 'pinterest'
+
+    await conn.sendMessage(
+      msg.chat,
+      {
+        image: { url: data.result.dl },
+        caption: `📌 *Pinterest*\n\n${title}`
+      },
+      { quoted: msg }
+    )
+
+  } catch (e) {
+    await conn.sendMessage(
+      msg.chat,
+      { text: `❌ Error:\n${e.message}` },
+      { quoted: msg }
+    )
+  }
+}
+
+handler.help = ['pindl <link>']
+handler.tags = ['download']
+handler.command = ['pindl', 'pinterestdl']
+
+export default handler
