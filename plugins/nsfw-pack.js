84344c1655d60c60b0f61306f6896f9afb54b47c
Angxlllll <angxlllll12@gmail.com>
2026-02-26 09:59:04 -0600

    Actualizar nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,21 +1,27 @@
-// deja créditos o serás funado por la legión lgtb 
+import { database } from '../lib/database.js'
 
 let handler = async (m, { conn }) => {
+
+    if (!database.data.groups?.[m.chat]?.nsfw)
+        return m.reply('🚫 El contenido NSFW está desactivado.\n\nUn admin debe usar *.on nsfw*.')
+
     await m.react('🕑')
 
     let txt = 'Pack🔥🔥🔥'
     let img = 'https://api.delirius.store/nsfw/girls'
 
-    await conn.sendMessage(m.chat, {
-        image: { url: img },
-        caption: txt
-    }, { quoted: m })
+    await conn.sendMessage(
+        m.chat,
+        { image: { url: img }, caption: txt },
+        { quoted: m }
+    )
 
     await m.react('✅')
 }
 
 handler.help = ['pack']
-handler.tags = ['misc']
+handler.tags = ['nsfw']
 handler.command = ['pack']
+handler.group = true
 
 export default handler
