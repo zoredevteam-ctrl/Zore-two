e6d5746b52ed617b921d3948749f02af8b383da9
DuarteXV <danielalejandrobasado@gmail.com>
2026-02-26 16:09:13 -0400

    Crear owner-autoadmin.jsj

---
+++ plugins/owner-autoadmin.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, participants  }) => {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
+}
+handler.help = ['autoadmin']
+handler.tags = ['owner']
+handler.command = ['autoadmin']
+
+handler.owner = true
+handler.group = true
+handler.botAdmin = true
+
+export default handler
