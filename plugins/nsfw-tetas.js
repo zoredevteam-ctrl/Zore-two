4cbf7cb06d99088baa50b91bb6dce0b103decb7b
Angxlllll <angxlllll12@gmail.com>
2026-02-26 09:59:15 -0600

    Actualizar nsfw-tetas.js

--- plugins/nsfw-tetas.js
+++ plugins/nsfw-tetas.js
@@ -1,11 +1,23 @@
+import { database } from '../lib/database.js'
+
 let handler = async (m, { conn }) => {
+
+    if (!database.data.groups?.[m.chat]?.nsfw)
+        return m.reply('🚫 El contenido NSFW está desactivado.\n\nUn admin debe usar *.on nsfw*.')
+
     let img = 'https://api.delirius.store/nsfw/boobs'
     let text = '*🍭 TETAS*'
 
-    await conn.sendMessage(m.chat, { image: { url: img }, caption: text }, { quoted: m })
+    await conn.sendMessage(
+        m.chat,
+        { image: { url: img }, caption: text },
+        { quoted: m }
+    )
+
     await m.react('✅')
 }
 
-handler.command = ['Tetas']
+handler.command = ['tetas']
+handler.group = true
 
 export default handler
