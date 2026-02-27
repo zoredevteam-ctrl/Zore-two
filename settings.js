a008f95f0f1cc33412c01e2d612171242607fdb8
DuarteXV <danielalejandrobasado@gmail.com>
2026-02-27 18:03:00 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ import fs from 'fs';
 const scriptPath = fileURLToPath(import.meta.url);
 
 global.owner = [
-['573229506110', 'Dev1', true],
-['51933000214'], 'dev2', true],
-['5215911153853', 'dev3', true],
+['573229506110', 'Dev1',],
+['51933000214'], 'dev2',],
+['5215911153853', 'dev3'],
 ['59175850453', 'Dev4'],
 ['584242773183', 'Dev5'],
 ['5493863447787', 'Dev6'],
