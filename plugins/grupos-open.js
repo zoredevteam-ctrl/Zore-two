c28e368bda5e6843845bb515486d93fac5354bbe
Carlos <supernumerovirtual@gmail.com>
2026-02-27 08:54:33 -0500

    Actualizar grupos-open.js

--- plugins/grupos-open.js
+++ plugins/grupos-open.js
@@ -1,5 +1,13 @@
 let handler = async (m, { conn }) => {
-    await conn.groupSettingUpdate(m.chat, 'not_announcement')
+    try {
+        await conn.groupSettingUpdate(m.chat, 'not_announcement')
+    } catch {
+        try {
+            await conn.groupSettingUpdate(m.chat, 'unlocked')
+        } catch {
+            return m.reply('*😔 No pude abrir el grupo.*')
+        }
+    }
     m.reply('*❀ Grupo abierto.*\n> Ya pueden volver a escribir sus ocurrencias.')
 }
 
