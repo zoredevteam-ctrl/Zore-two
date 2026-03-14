b2117cdc5395df3a6a9ccce304b721bb8e9d6f80
zoredevteam-ctrl <zoredevteam@gmail.com>
2026-03-13 23:32:39 -0500

    Actualizar tools-pp.js

--- plugins/tools-pp.js
+++ plugins/tools-pp.js
@@ -1,18 +1,37 @@
-let handler = async (m, { conn, args }) => {
-    await m.react('🍬')
+let handler = async (m, { conn }) => {
+    try {
+        await m.react('🍬')
 
-    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-    let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://telegra.ph/file/999999999999999999999.jpg') // fallback si no tiene foto
+        let who = m.mentionedJid[0] 
+            ? m.mentionedJid[0] 
+            : m.quoted 
+                ? m.quoted.sender 
+                : m.sender
 
-    let caption = `💗 *Foto de perfil de ${await conn.getName(who)}* 🌸\n\n` +
-                  `¡Mira qué cute darling\~! 💕`
+        let pp = await conn.profilePictureUrl(who, 'image').catch(() => 'https://telegra.ph/file/999999999999999999999.jpg')
 
-    await conn.sendMessage(m.chat, { image: { url: pp }, caption }, { quoted: m })
-    await m.react('💗')
+        let name = await conn.getName(who) || 'Darling'
+
+        let caption = `💗 *Foto de perfil de ${name}* 🌸\n\n` +
+                      `¡Mira qué cute darling\~! 💕`
+
+        await conn.sendMessage(m.chat, {
+            image: { url: pp },
+            caption: caption,
+            mentions: [who]
+        }, { quoted: m })
+
+        await m.react('💗')
+
+    } catch (e) {
+        console.error('PP ERROR:', e)
+        await m.react('💔')
+        m.reply('💔 Uy darling... no pude cargar la foto esta vez\~ Prueba otra vez')
+    }
 }
 
-handler.help = ['pp', 'foto @user']
+handler.help = ['pp @user', 'foto @user']
 handler.tags = ['tools']
-handler.command = ['pp', 'foto', 'profilepic']
+handler.command = ['pp', 'foto', 'profilepic', 'ppuser']
 
 export default handler
