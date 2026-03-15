416150346109e0c427bda720e358c6219b74231b
zoredevteam-ctrl <zoredevteam@gmail.com>
2026-03-15 12:37:06 -0500

    Actualizar nsfw-lesbian.js

--- plugins/nsfw-lesbian.js
+++ plugins/nsfw-lesbian.js
@@ -1,30 +1,16 @@
-import fetch from 'node-fetch'
+import { database } from '../lib/database.js'
 
 let handler = async (m, { conn }) => {
-    if (!global.db.data.chats[m.chat].nsfw && m.isGroup) {
+    if (!database.data.groups?.[m.chat]?.nsfw) {
         return m.reply('🚫 El contenido NSFW está desactivado en este grupo.\n\nUn admin puede activarlo con *#nable nsfw on*')
     }
 
-    await m.react('🍬')
+    let img = 'https://nekobot.xyz/api/image?type=lesbian'
 
-    try {
-        const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=lesbian&json=1&limit=1`
-        const res = await fetch(url)
-        const json = await res.json()
-
-        if (!json || json.length === 0) throw new Error('No image')
-
-        const imageUrl = json[0].file_url
-
-        await conn.sendMessage(m.chat, {
-            image: { url: imageUrl },
-            caption: '*LESBIAN*'
-        }, { quoted: m })
-
-    } catch (e) {
-        console.error(e)
-        m.reply('❌ No se pudo obtener imagen. Prueba de nuevo.')
-    }
+    await conn.sendMessage(m.chat, {
+        image: { url: img },
+        caption: '*LESBIAN*'
+    }, { quoted: m })
 }
 
 handler.help = ['lesbian']
