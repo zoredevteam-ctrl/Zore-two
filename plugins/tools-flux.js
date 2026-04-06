d1bd1836d1f8988fe26844f340f29628729764ef
Carlos <supernumerovirtual@gmail.com>
2026-04-05 22:09:27 -0400

    Actualizar tools-flux.js

--- plugins/tools-flux.js
+++ plugins/tools-flux.js
@@ -24,7 +24,7 @@ const handler = async (msg, { conn, args, usedPrefix, command }) => {
   )
 
   try {
-    const api = `https://nex-magical.vercel.app/ai/flux?prompt=${encodeURIComponent(query)}`
+    const api = `https://nex-magical.vercel.app/ai/flux?prompt=${encodeURIComponent(query)}&apikey=NEX-Magicalofc`
     const res = await axios.get(api, { responseType: 'arraybuffer' })
 
     await conn.sendMessage(
