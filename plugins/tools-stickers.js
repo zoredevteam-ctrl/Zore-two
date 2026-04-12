af0732fd35f7edd9a7cc74deaae345a8e442451c
yosue891 <yosueortega630@gmail.com>
2026-04-12 19:32:38 -0400

    Update tools-stickers.js

--- plugins/tools-stickers.js
+++ plugins/tools-stickers.js
@@ -1,4 +1,5 @@
 import { Sticker, StickerTypes } from 'wa-sticker-formatter'
+import { downloadMediaMessage } from '@whiskeysockets/baileys'
 
 const handler = async (m, { conn, args, command, usedPrefix }) => {
     try {
@@ -10,35 +11,33 @@ const handler = async (m, { conn, args, command, usedPrefix }) => {
             return m.reply(`🌸 ¿Y mi media darling? 💗\nResponde a una imagen/video/gif con\n*${usedPrefix}${command}*`)
         }
 
-        if (!/image|video/.test(mime)) {
-            await m.react('💔')
-            return m.reply('💔 Solo imágenes, videos y gifs se pueden convertir, mi amor\~')
-        }
-
         await m.react('🍬')
 
-        let img = await q.download?.()
-        if (!img) return m.reply('💔 No pude descargar la media, darling...')
+        let media = await downloadMediaMessage(q, 'buffer', {}, {
+            reuploadRequest: conn.updateMediaMessage
+        })
 
-        let pack = args.length ? args.join(' ') : (global.packname || '💗 𝒁𝒆𝒓𝒐 𝑻𝒘𝒐 💗')
-        let author = global.author || '© Zore Two'
+        if (!media) throw new Error('No se pudo descargar la media')
 
-        const sticker = new Sticker(img, {
+        let pack = args[0] || global.packname || '💗 𝒁𝒆𝒓𝒐 𝑻𝒘𝒐 💗'
+        let author = args[1] || global.author || '© Zore Two'
+
+        const sticker = new Sticker(media, {
             pack: pack,
             author: author,
             type: StickerTypes.FULL,
             categories: ['💗'],
-            quality: 70
+            quality: 60
         })
 
-        const stickerBuffer = await sticker.toBuffer()
-        await conn.sendMessage(m.chat, { sticker: stickerBuffer }, { quoted: m })
+        const buffer = await sticker.toBuffer()
+        await conn.sendMessage(m.chat, { sticker: buffer }, { quoted: m })
         await m.react('💗')
 
     } catch (e) {
         console.error(e)
         await m.react('💔')
-        m.reply('💔 Darling, algo salió mal... inténtalo de nuevo~')
+        m.reply('💔 Darling, mi sistema no reconoce este comando o la media falló... prueba de nuevo~')
     }
 }
 
